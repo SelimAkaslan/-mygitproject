aac14bb2ebb16a924ecee9095a24907acfdbd290
selim akaslan <sakaslan6957@gmail.com>
2021-04-04 20:11:45 +0200

    7.commit

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -9,6 +9,12 @@ public class MyGitClass {
 		System.out.println("conflict commit");
 		System.out.println("4. commit");
 		System.out.println("My 5 commit");
+		int sayi = 12;
+		for (int i = 0; i < sayi; i++) {
+			System.out.print(i+",");
+			
+		}
+		System.out.println();
 		
 		System.out.println("My 6. conflict commit");
 		System.out.println("Uzak sunucudaki kodlarim");
